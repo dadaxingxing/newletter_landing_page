c4595f1da599eadb4bdd61d3d5c436100632d9ea
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-01-02 11:53:16 -0800

    added beehive subscriber form, could not get form to update dashboard in beehive

---
+++ src/component/button.jsx
@@ -0,0 +1,23 @@
+
+
+const BeehiveEmbed = () => {
+    return (
+        <div>
+            <iframe
+                src="https://embeds.beehiiv.com/ac2db25e-2eae-4d38-899f-024abd6f0ffa?slim=true"
+                data-test-id="beehiiv-embed"
+                height="52"
+                frameBorder="0"
+                scrolling="no"
+                style={{
+                    margin: 0,
+                    borderRadius: '0px !important',
+                    backgroundColor: 'transparent',
+                }}
+                title="Beehive Embed"
+            ></iframe>
+        </div>
+    );
+};
+
+export default BeehiveEmbed;
