8774a6b44d0fae0ad7b6b0e8612ab51d0e672a2d
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-01-02 11:20:22 -0800

    finished the headline component

---
+++ src/component/headline.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+const Head_line = () => {
+    return (
+        <div className='header'>
+            Go <span className='viral'>viral</span>
+            <br />on <u>Instagram</u>
+        </div>
+    )
+};
+
+export default Head_line;
