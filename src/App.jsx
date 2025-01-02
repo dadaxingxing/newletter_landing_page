9e89020f0f2bdf2bc935ecfc9b03720fea21660a
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-01-02 11:29:58 -0800

    added paragraph component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import Head_line from './component/headline.jsx'
-
+import Paragraph from './component/paragraph.jsx'
 
 function App() {
 
   return (
-    <Head_line />
+    <></>
   )
 };
 
---
+++ src/component/paragraph.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+const Paragraph = () => {
+    return (
+        <div className='paragraph'>
+            How I generated 65k followers and 25+ million views in 2 years. Enter your email below to get
+            <br /> the 5 biggest mistakes I learned while growing an audience.
+        </div>
+    )
+};
+
+export default Paragraph;
+
