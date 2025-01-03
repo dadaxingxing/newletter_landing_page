b24661feef6d98293981df950b3206d4ff4b22e9
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-01-03 07:50:50 -0800

    added mockup and also finished desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css'
-import Head_line from './component/headline.jsx'
-import Paragraph from './component/paragraph.jsx'
-import BeehiveEmbed from './component/button.jsx'
-
+import './App.css';
+import Head_line from './component/headline.jsx';
+import Paragraph from './component/paragraph.jsx';
+import BeehiveEmbed from './component/button.jsx';
+import MockUp from './assets/phone_mockup.svg';
 
 function App() {
 
   return (
-    <>
-    <Head_line/>
-    <Paragraph/>
-    <BeehiveEmbed/>
-    </>
+    <div className='outer_container'>
+      <div className='inner_container'>
+        <Head_line/>
+        <Paragraph/>
+        <BeehiveEmbed/>
+      </div>
+      <img src={MockUp} alt='My Mockup' className='mockup'></img>
+    </div>
+
   )
 };
 
-export default App
+export default App;
--- src/component/button.jsx
+++ src/component/button.jsx
@@ -1,8 +1,8 @@
-
+import '../App.css';
 
 const BeehiveEmbed = () => {
     return (
-        <div>
+        <div className='sub_button'>
             <iframe
                 src="https://embeds.beehiiv.com/ac2db25e-2eae-4d38-899f-024abd6f0ffa?slim=true"
                 data-test-id="beehiiv-embed"
